0771fee7def6ebc4b9139fcb2c71eaf578937f7a
Rasmus <krewli@hotmail.com>
2016-11-03 14:58:52 +0100

    Added most missing things. ROC curve needs to be investigated.

--- lab1/src/assignment1/solution.R
+++ lab1/src/assignment1/solution.R
@@ -1,13 +1,23 @@
 library(kknn)
+library(caret)
+library(ggplot2)
 
+## 1
 data <- read.csv("../../data/spambase.csv", sep=",", header=TRUE)
 
 n <- nrow(data)
 set.seed(12345)
 id <- sample(1:n, floor(n * 0.5))
+
 train <- data[id,]
 test <- data[-id,]
 
+train_labels <- factor(train[, ncol(train)], levels=c(0, 1), labels=c("non-spam", "spam"))
+test_labels <- factor(test[, ncol(test)], levels=c(0, 1), labels=c("non-spam", "spam"))
+
+threshold <- 0.5
+
+## 2
 knearest <- function(data, k, newdata) {
     stopifnot(k > 0)
     
@@ -20,43 +30,80 @@ knearest <- function(data, k, newdata) {
     test <- test / sqrt(rowSums(test^2))
 
     cosine_sim <- train %*% t(test)
-    distance_mat <- 1 - cosine_sim
+    cosine_dis <- 1 - cosine_sim
 
+    ordering <- as.matrix(t(apply(cosine_dis, 2, order))[, 1:k])
 
-    
-    ordering <- as.matrix(apply(distance_mat, 2, order)[1:k,])
-    print(head(ordering))
-    
-    predicted_labels <- as.matrix(apply(ordering, 2, function(x) {
+    predicted <- as.matrix(apply(ordering, 1, function(x) {
         mean(train_labels[x])
     }))
-    predicted_labels
+
+    predicted
 }
 
-threshold <- 0.5
+predicted_result <- function(predicted, actual, threshold) {
+    predicted <- as.numeric(predicted > threshold)
+    predicted <- factor(predicted, levels=c(0, 1), labels=c("non-spam", "spam"))
+    table(actual, predicted)
+}
 
-predicted_labels <- knearest(train, 5, test)
-predicted_labels <- as.numeric(predicted_labels > threshold)
-sum(predicted_labels)
-predicted_labels <- factor(predicted_labels, levels=c(0, 1), labels=c("non-spam", "spam"))
-table(predicted_labels, test[, ncol(test)])
-table(predicted_labels, train[, ncol(train)])
+## Testing that the implementation seems to be correct
+predicted <- knearest(train, 1, train)
+predicted_result(predicted, train_labels, threshold)
 
-predicted_labels <- knearest(train, 1, test)
-predicted_labels <- as.numeric(predicted_labels > threshold)
-predicted_labels <- factor(predicted_labels, levels=c(0, 1), labels=c("non-spam", "spam"))
-table(predicted_labels, test[, ncol(test)])
+predicted <- knearest(test, 1, test)
+predicted_result(predicted, test_labels, threshold)
 
+## 3
+predicted <- knearest(train, 5, test)
+predicted_result(predicted, test_labels, threshold)
 
-predicted_labels <- knearest(train, 1, train)
-predicted_labels
+## 4
+predicted <- knearest(train, 1, test)
+predicted_result(predicted, test_labels, threshold)
 
-predicted_labels <- as.numeric(predicted_labels > threshold)
-predicted_labels <- factor(predicted_labels, levels=c(0, 1), labels=c("non-spam", "spam"))
+## 5
+kknn.fit <- kknn(Spam ~ ., train=train, test=test, distance=2, k=5)
+predicted <- fitted(kknn.fit)
+predicted_result(predicted, test_labels, threshold)
 
-predicted_labels
-table(predicted_labels, train[, ncol(train)])
+## 6
+threshold <- seq(0.05, 0.95, by=0.05)
 
+predicted_knearest <- knearest(train, 5, test)
 
+kknn.fit <- kknn(Spam ~ ., train=train, test=test, distance=2, k=5)
+predicted_kknn<- fitted(kknn.fit)
 
-?kknn
+knearest_sensitivity <- rep(0, length(threshold))
+knearest_specificity <- rep(0, length(threshold))
+
+kknn_sensitivity <- rep(0, length(threshold))
+kknn_specificity <- rep(0, length(threshold))
+
+for (i in 1:length(threshold)) {
+    knearest_prediction <- predicted_result(predicted_knearest, test_labels, threshold[i])
+    knearest_sensitivity[i] <- sensitivity(knearest_prediction)
+    knearest_specificity[i] <- specificity(knearest_prediction)
+
+    kknn_prediction <- predicted_result(predicted_kknn, test_labels, threshold[i])
+    kknn_sensitivity[i] <- sensitivity(kknn_prediction)
+    kknn_specificity[i] <- specificity(kknn_prediction)
+}
+
+knearest_data <- data.frame(x=(1 - knearest_specificity), y=knearest_sensitivity,
+                            label=as.character(threshold))
+knearest_data
+
+kknn_data <- data.frame(x=(1 - kknn_specificity), y=kknn_sensitivity,
+                        label=as.character(threshold))
+kknn_data
+
+ggplot() + ggtitle("ROC Curve") +
+    xlab("False Positive Rate (1 - specificity)") +
+    ylab("True Positive Rate (sensitivity)") +
+    geom_line(data=knearest_data, aes(x, y), color="red") +
+    geom_line(data=kknn_data, aes(x, y), color="blue") +
+    scale_x_continuous(limits = c(0, 1)) + scale_y_continuous(limits=c(0, 1)) +
+    theme(plot.title=element_text(hjust=0.5))
+
