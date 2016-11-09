a6dd50077bb0972a18d106f9e415a3ca62a604c6
Rasmus <krewli@hotmail.com>
2016-11-09 12:43:13 +0100

    Started on assignment 1.

---
+++ lab2/assignment1/solution.R
@@ -0,0 +1,79 @@
+best_subset_selection <- function(X, y, folds) {
+    n <- nrow(X)
+    p <- ncol(X)
+
+    sampled_idx <- sample(1:n, n)
+    sets <- cross_validation_sets(n, folds)
+    best_features <- list()
+
+    for (j in 1:p) {
+        feature_combinations <- combinations(p, j)
+        errors <- c()
+        for (feature_idx in 1:nrow(feature_combinations)) {
+            features <- feature_combinations[feature_idx, ] == 1
+            current_errors <- c()
+            for (i in 1:folds) {
+                test_validation_idx <- sets[i, 1]:sets[i, 2]
+                test_idx <- sampled_idx[test_validation_idx]
+                training_idx <- sampled_idx[(1:n)[-test_validation_idx]]
+
+                lmfit <- linear_regression(as.matrix(X[training_idx, features]), y[training_idx],
+                                           as.matrix(X[test_idx, features]), y[test_idx])
+                current_errors <- c(current_errors, lmfit$SSE)
+            }
+            errors <- c(errors, mean(current_errors))
+        }
+        best_features[[j]] <- list(features=feature_combinations[which.min(errors),],
+                                   SSE=min(errors))
+    }
+
+    best_features
+}
+
+cross_validation_sets <- function(n, folds) {
+    set_size <- as.integer(n / folds)
+    remaining <- n - folds * set_size
+
+    idx <- matrix(0, nrow=folds, ncol=2)
+    idx[1, 1] <- 1
+    idx[1, 2] <- set_size
+
+    for (i in 2:folds) {
+        idx[i, 1] <- idx[i - 1, 2] + 1
+        idx[i, 2] <- idx[i, 1] + (set_size - 1)
+
+        if (remaining > 0) {
+            idx[i, 2] <- idx[i, 2] + 1
+            remaining <- remaining - 1
+        }
+    }
+
+    idx
+}
+
+linear_regression <- function(X_train, y_train, X_test, y_test) {   
+    coefficients <- solve(t(X_train) %*% X_train) %*% t(X_train) %*% y_train
+    coefficients <- as.vector(coefficients)
+    fitted_values <- X_test %*% coefficients
+    SSE <- sum((y_test- fitted_values)^2)
+    list(coefficients=coefficients, fitted_values=fitted_values, SSE=SSE)
+}
+
+combinations <- function(n, m) {
+    t(apply(combn(1:n, m=m), 2, function(x) replace(rep(0, n), x, 1)))
+}
+
+data <- swiss
+x <- data[, -1]
+y <- data[, 1]
+folds <- 5
+set.seed(12345)
+result <- best_subset_selection(x, y, folds)
+result
+
+best_setting <- result[[which.min(sapply(result, function(x) x$SSE))]]
+colnames(x)[best_setting$features == 1]
+
+lmfit <- linear_regression(as.matrix(x[, best_setting$features == 1]), y,
+                           as.matrix(x[, best_setting$features == 1]), y)
+lmfit$coefficients
