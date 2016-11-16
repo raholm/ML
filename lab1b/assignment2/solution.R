614ac2eb0f3f098d4d62e654dd0d356b58974fa2
Rasmus Holm <rasmus.holm723@gmail.com>
2016-11-16 15:43:42 +0100

    Done with most of the code for assignment 2.

--- lab1b/assignment2/solution.R
+++ lab1b/assignment2/solution.R
@@ -3,6 +3,8 @@ library(ggplot2)
 library(readxl)
 library(reshape2)
 library(mgcv)
+library(grid)
+library(gridExtra)
 
 data <- read_excel("../data/Influenza.xlsx")
 ## ---- end-of-assign2-init
@@ -16,8 +18,7 @@ ggplot(plot_data) +
 
 ## 2
 ## ---- assign2-2
-gamfit <- gam(Mortality ~ Year + s(Week), family=gaussian(), data=data, method="GCV.Cp")
-coefficients <- coef(gamfit)
+gamfit <- gam(Mortality ~ Year + s(Week), family=gaussian, data=data, method="GCV.Cp")
 ## ---- end-of-assign2-2
 
 ## 3
@@ -31,35 +32,44 @@ ggplot(plot_data) +
     geom_line(aes(x=Time, y=Mortality, color=Data))
 ## ---- end-of-assign2-3-plot
 
+## ---- assign2-3-summary
 summary(gamfit)
+## ---- end-of-assign2-3-summary
+
+## ---- assign2-3-spline
 plot(gamfit)
-coef(gamfit)
+## ---- end-of-assign2-3-spline
 
 ## 4
+## ---- assign2-4
 k <- length(unique(data$Week)) - 1
+penalty_values <- c(0, 10, 1000, 100000)
 
-gamfit1 <- gam(Mortality ~ Year + s(Week, k=k, sp=100000000),
-              family=gaussian, data=data, method="GCV.Cp")
-gamfit2 <- gam(Mortality ~ Year + s(Week, k=k, sp=0),
-              family=gaussian, data=data, method="GCV.Cp")
+plots <- list()
 
-summary(gamfit1)
-summary(gamfit2)
+for (i in 1:length(penalty_values)) {
+    fit <- gam(Mortality ~ Year + s(Week, k=k, sp=penalty_values[i]),
+               family=gaussian, data=data, method="GCV.Cp")
 
-plot_data <- data.frame(Time=data$Time, Actual=data$Mortality,
-                        EstimateHigh=gamfit1$fitted.values, EstimateLow=gamfit2$fitted.values)
-plot_data <- melt(plot_data, id="Time", value.name="Mortality", variable.name="Data")
+    title <- paste("Params (k=", k, ",sp=", penalty_values[i], ")", sep="")
 
-ggplot(plot_data) +
-    geom_line(aes(x=Time, y=Mortality, color=Data))
+    plot_data <- data.frame(Time=data$Time, Actual=data$Mortality, Estimate=fitted(fit))
+    plot_data <- melt(plot_data, id="Time", value.name="Mortality", variable.name="Data")
 
+    plots[[i]] <- ggplot(plot_data) +
+        geom_line(aes(x=Time, y=Mortality, color=Data), show.legend=FALSE) +
+        ggtitle(title) +
+        theme(axis.text=element_blank())
+}
 
-plot(gamfit, residuals=TRUE)
+do.call(grid.arrange, c(plots, list(ncol=2)))
+## ---- end-of-assign2-4
 
 ## 5
 ## ---- assign2-5
+gamfit <- gam(Mortality ~ Year + s(Week), family=gaussian, data=data, method="GCV.Cp")
 residuals <- resid(gamfit)
-plot_data <- data.frame(Time=data$Time, Influenza=data$Influenza, Residuals=residuals)
+plot_data <- data.frame(Time=data$Time, Residuals=residuals, Influenza=data$Influenza)
 plot_data <- melt(plot_data, id="Time", value.name="Value", variable.name="Data")
 
 ggplot(plot_data) +
@@ -67,7 +77,16 @@ ggplot(plot_data) +
 ## ---- end-of-assign2-5
 
 ## 6
-gamfit <- gam(Mortality ~ s(Year) + s(Week) + s(Influenza), data=data)
+## ---- assign2-6
+gamfit <- gam(Mortality ~ s(Year, k=length(unique(data$Year)) - 1) +
+                  s(Week, k=length(unique(data$Week)) - 1) +
+                  s(Influenza, k=length(unique(data$Influenza))),
+              data=data)
+summary(gamfit)
 
-data$Influenza
-data$Year
+plot_data <- data.frame(Time=data$Time, Actual=data$Mortality, Estimate=fitted(gamfit))
+plot_data <- melt(plot_data, id="Time", value.name="Value", variable.name="Data")
+
+ggplot(plot_data) +
+    geom_line(aes(x=Time, y=Value, color=Data))
+## ---- end-of-assign2-6
