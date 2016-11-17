960f7e16e0fd9befab38f399ef3c12e3db0a968b
Rasmus <krewli@hotmail.com>
2016-11-17 21:08:56 +0100

    Basically done with the lda.

--- lab3/assignment1/solution.R
+++ lab3/assignment1/solution.R
@@ -23,14 +23,6 @@ plda <- function(X, y) {
     result <- list()
     sigmahat <- matrix(0, nrow=length(labels), ncol=length(labels))
 
-    ## means <- aggregate(X, list(y), mean)
-    ## covs <- by(X, list(y), cov)
-    ## lengths <- by(X, list(y), nrow)
-
-    ## cov_mats <- lapply(1:length(lengths), function(i) covs[[i]] * lengths[[i]])
-    ## covhat <- Reduce("+", mats) / sum(lengths)
-
-
     for (i in 1:length(labels)) {
         data <- X[which(y == labels[i]),]
         mean <- as.matrix(colMeans(data), nrow=p)
@@ -61,41 +53,78 @@ plda <- function(X, y) {
     list(result, sigmahat)
 }
 
-X <- cbind(data$CL, data$RW)
+plda.new <- function(X, y) {
+    n <- nrow(X)
+    p <- ncol(X)
+
+    labels <- unique(y)
+    priors <- table(y) / length(y)
+
+    means <- aggregate(X, list(y), mean)
+    means <- as.matrix(means[, -1], ncol=p)
+
+    lengths <- by(X, list(y), nrow)
+
+    cov_mats <- by(X, list(y), cov)
+    cov_mats <- lapply(1:length(lengths), function(i) {
+        cov_mats[[i]] * lengths[[i]]
+    })
+
+    sigma <- as.matrix(Reduce("+", cov_mats) / sum(lengths), nrow=p)
+    sigma_inv <- solve(sigma)
+
+    w0 <- sapply(1:length(labels), function(i) {
+        -(1 / 2) * t(means[i,]) %*% sigma_inv %*% means[i,] + log(priors[i])
+    })
+
+    w1 <- sapply(1:length(labels), function(i) {
+        sigma_inv %*% means[i, ]
+    })
+
+    list(w0=w0, w1=w1, sigma=sigma)
+}
+
+
+X <- cbind(data$RW, data$CL)
 y <- data$sex
 
-fit <- plda(X, y)
-result <- fit[[1]]
-sigma <- fit[[2]]
+## fit <- plda(X, y)
+## result <- fit[[1]]
+## sigma <- fit[[2]]
 
-result[[1]]$w0
-result[[1]]$w1
-result[[1]]$discriminant
+## result[[1]]$w0
+## result[[1]]$w1
+## result[[1]]$mean
+## result[[2]]$w0
+## result[[2]]$w1
+## result[[2]]$mean
 
-solve(sigma) %*% (result[[1]]$mean - result[[2]]$mean)
+result <- plda.new(X, y)
 
+w1 <- result$w1[, 2] - result$w1[, 1]
+w0 <- result$w0[2] - result$w0[1]
 
-a <- list(w10 = result[[1]]$w0, w11=result[[1]]$w1, w20 = result[[2]]$w0, w21=result[[2]]$w1)
+intercept <- -w0 / w1[2]
+slope <- -w1[1] / w1[2]
+predicted <- factor(as.numeric((w0 + w1 %*% t(X)) > 0), levels=c(0, 1), labels=c("Female", "Male"))
 
-b <- a$w10 - a$w20
-w <- a$w11 - a$w21
-b
-w
+## Actual Data
+plot_data <- data.frame(RW=data$RW, CL=data$CL, class=data$sex)
+line_data <- data.frame(intercept=intercept, slope=slope)
 
-intercept <- -b / w[2]
-slope <- -w[1] / w[2]
+ggplot() +
+    geom_point(data=plot_data, aes(x=RW, y=CL, color=class)) +
+    geom_abline(data=line_data, intercept=intercept, slope=slope,
+                color="black", linetype="dotted", size=1)
 
-b
-s
+## Predicted Data
+plot_data <- data.frame(RW=data$RW, CL=data$CL, class=predicted)
+line_data <- data.frame(intercept=intercept, slope=slope)
 
-solution <- as.numeric(t(w) %*% t(X) + b)
-solution
-
-yfit <- solution > 0
-yfit
-
-plot(cbind(X[,1], X[, 2]))
-abline(intercept, slope)
+ggplot() +
+    geom_point(data=plot_data, aes(x=RW, y=CL, color=class)) +
+    geom_abline(data=line_data, intercept=intercept, slope=slope,
+                color="black", linetype="dotted", size=1)
 
 ## 3
 
