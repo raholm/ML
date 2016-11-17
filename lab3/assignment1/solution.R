06c6b203606c7164f192922bb5d8a1db717d1841
Rasmus Holm <rasmus.holm723@gmail.com>
2016-11-17 17:34:52 +0100

    Kept working on assigmnet 1.

--- lab3/assignment1/solution.R
+++ lab3/assignment1/solution.R
@@ -14,6 +14,7 @@ ggplot(data) +
 plda <- function(X, y) {
     n <- nrow(X)
     p <- ncol(X)
+
     labels <- unique(y)
     priors <- table(y) / length(y)
 
@@ -21,6 +22,14 @@ plda <- function(X, y) {
 
     result <- list()
     sigmahat <- matrix(0, nrow=length(labels), ncol=length(labels))
+
+    ## means <- aggregate(X, list(y), mean)
+    ## covs <- by(X, list(y), cov)
+    ## lengths <- by(X, list(y), nrow)
+
+    ## cov_mats <- lapply(1:length(lengths), function(i) covs[[i]] * lengths[[i]])
+    ## covhat <- Reduce("+", mats) / sum(lengths)
+
 
     for (i in 1:length(labels)) {
         data <- X[which(y == labels[i]),]
@@ -41,13 +50,12 @@ plda <- function(X, y) {
     for (i in 1:length(result)) {
         class <- result[[i]]
 
-        first <- class$data %*% solve(sigmahat) %*% class$mean
-        second <- as.numeric((1 / 2) * t(class$mean) %*% solve(sigmahat) %*% class$mean)
-        third <- log(class$priors)
+        w0 <- as.numeric(-(1 / 2) * t(class$mean) %*% solve(sigmahat) %*% class$mean + log(class$priors))
+        w1 <- solve(sigmahat) %*% class$mean
 
-        discriminant <- first - second + third
-
-        result[[i]]$discriminant <- discriminant
+        result[[i]]$discriminant <- w0 + class$data %*% w1
+        result[[i]]$w0 <- w0
+        result[[i]]$w1 <- w1
     }
 
     list(result, sigmahat)
@@ -59,6 +67,35 @@ y <- data$sex
 fit <- plda(X, y)
 result <- fit[[1]]
 sigma <- fit[[2]]
+
+result[[1]]$w0
+result[[1]]$w1
+result[[1]]$discriminant
+
+solve(sigma) %*% (result[[1]]$mean - result[[2]]$mean)
+
+
+a <- list(w10 = result[[1]]$w0, w11=result[[1]]$w1, w20 = result[[2]]$w0, w21=result[[2]]$w1)
+
+b <- a$w10 - a$w20
+w <- a$w11 - a$w21
+b
+w
+
+intercept <- -b / w[2]
+slope <- -w[1] / w[2]
+
+b
+s
+
+solution <- as.numeric(t(w) %*% t(X) + b)
+solution
+
+yfit <- solution > 0
+yfit
+
+plot(cbind(X[,1], X[, 2]))
+abline(intercept, slope)
 
 ## 3
 
