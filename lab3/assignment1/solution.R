389910ee4a3bc6140d9d1e3927fcba9cac50d6cc
Rasmus Holm <rasmus.holm723@gmail.com>
2016-11-18 14:41:33 +0100

    Finished up the code for assignment 1 and 2.

--- lab3/assignment1/solution.R
+++ lab3/assignment1/solution.R
@@ -1,16 +1,18 @@
 ## ---- assign1-init
 library(ggplot2)
+library(glmnet)
 
 data <- read.csv("../data/australian-crabs.csv", sep=",")
 ## ---- end-of-assign1-init
 
 ## 1
-## ---- assign1-1-plot
+## ---- assign1-1
 ggplot(data) +
     geom_point(aes(x=RW, y=CL, color=sex))
-## ---- end-of-assign1-1-plot
+## ---- end-of-assign1-1
 
 ## 2
+## ---- assign1-2
 LDA <- function(X, y) {
     n <- nrow(X)
     p <- ncol(X)
@@ -39,9 +41,11 @@ LDA <- function(X, y) {
         sigma_inv %*% means[i, ]
     })
 
+    names(w0) <- levels(labels)
+    colnames(w1) <- levels(labels)
+
     list(w0=w0, w1=w1, sigma=sigma)
 }
-
 
 X <- cbind(data$RW, data$CL)
 y <- data$sex
@@ -56,16 +60,6 @@ slope <- -w1[1] / w1[2]
 predicted <- as.numeric((w0 + w1 %*% t(X)) > 0)
 predicted <- factor(predicted, levels=c(0, 1), labels=c("Female", "Male"))
 
-## Actual Data
-plot_data <- data.frame(RW=data$RW, CL=data$CL, class=data$sex)
-line_data <- data.frame(intercept=intercept, slope=slope)
-
-ggplot() +
-    geom_point(data=plot_data, aes(x=RW, y=CL, color=class)) +
-    geom_abline(data=line_data, intercept=intercept, slope=slope,
-                color="black", linetype="dotted", size=1)
-
-## Predicted Data
 plot_data <- data.frame(RW=data$RW, CL=data$CL, class=predicted)
 line_data <- data.frame(intercept=intercept, slope=slope)
 
@@ -73,7 +67,37 @@ ggplot() +
     geom_point(data=plot_data, aes(x=RW, y=CL, color=class)) +
     geom_abline(data=line_data, intercept=intercept, slope=slope,
                 color="black", linetype="dotted", size=1)
+## ---- end-of-assign1-2
 
 ## 3
+## ---- assign1-3
+plot_data <- data.frame(RW=data$RW, CL=data$CL, class=data$sex)
+line_data <- data.frame(intercept=intercept, slope=slope)
+
+ggplot() +
+    geom_point(data=plot_data, aes(x=RW, y=CL, color=class)) +
+    geom_abline(data=line_data, intercept=intercept, slope=slope,
+                color="black", linetype="dotted", size=1)
+## ---- end-of-assign1-3
 
 ## 4
+## ---- assign1-4
+logistic_data <- data.frame(sex=as.numeric(data$sex) - 1, RW=data$RW, CL=data$CL)
+
+glmfit <- glm(sex ~ RW + CL, data=logistic_data, family=binomial(link=logit))
+coefficients <- coef(glmfit)
+
+predicted <- as.numeric(glmfit$fitted.values > 0.5)
+predicted <- factor(predicted, levels=c(0, 1), labels=c("Female", "Male"))
+
+intercept <- -coefficients[1] / coefficients[3]
+slope <- -coefficients[2] / coefficients[3]
+
+plot_data <- data.frame(RW=data$RW, CL=data$CL, class=predicted)
+line_data <- data.frame(intercept=intercept, slope=slope)
+
+ggplot() +
+    geom_point(data=plot_data, aes(x=RW, y=CL, color=class)) +
+    geom_abline(data=line_data, intercept=intercept, slope=slope,
+                color="black", linetype="dotted", size=1)
+## ---- end-of-assign1-4
---
+++ lab3/assignment2/solution.R
@@ -0,0 +1,131 @@
+## ---- assign2-init
+library(gdata)
+library(tree)
+library(partykit)
+library(ggplot2)
+library(reshape2)
+library(e1071)
+
+data_division <- function(n, training, test, validation) {
+    indices <- 1:n
+
+    train <- sample(indices, floor(n * 0.5))
+    test <- sample(indices[-train], floor(n * test))
+    validation <- indices[-c(train, test)]
+
+    list(train=train, test=test, validation=validation)
+}
+
+data <- read.xls("../data/creditscoring.xls")
+## ---- end-of-assign2-init
+
+## 1
+## ---- assign2-1
+set.seed(12345)
+indices <- data_division(nrow(data), 0.5, 0.25, 0.25)
+
+train <- data[indices$train,]
+test <- data[indices$test,]
+validation <- data[indices$validation,]
+## ---- end-of-assign2-1
+
+## 2
+## ---- assign2-2
+prediction.tree <- function(model, X, y) {
+    predicted <- predict(model, X)
+    predicted <- factor(ifelse(predicted[, 1] > predicted[, 2], 0, 1), levels=c(0, 1), labels=c("bad", "good"))
+
+    confusion_matrix <- table(pred=predicted, true=y)
+    list(confusion_matrix=confusion_matrix,
+         misclassification_rate= 1 - sum(diag(confusion_matrix)) / sum (confusion_matrix))
+}
+
+dtreefit <- tree(good_bad ~ ., data=train, split="deviance")
+gtreefit <- tree(good_bad ~ ., data=train, split="gini")
+## ---- end-of-assign2-2
+
+## ---- assign2-2-dtree-train
+prediction.tree(dtreefit, train[, -ncol(train)], train$good_bad)
+## ---- end-of-assign2-2-dtree-train
+
+## ---- assign2-2-dtree-test
+prediction.tree(dtreefit, test[, -ncol(test)], test$good_bad)
+## ---- end-of-assign2-2-dtree-test
+
+## ---- assign2-2-gtree-train
+prediction.tree(gtreefit, train[, -ncol(train)], train$good_bad)
+## ---- end-of-assign2-2-gtree-train
+
+## ---- assign2-2-gtree-test
+prediction.tree(gtreefit, test[, -ncol(test)], test$good_bad)
+## ---- end-of-assign2-2-gtree-test
+
+## 3
+## ---- assign2-3
+leaves <- 2:summary(dtreefit)[4]$size
+train_score <- rep(0, max(leaves))
+validation_score <- rep(0, max(leaves))
+
+for(i in leaves) {
+    prunedTree <- prune.tree(dtreefit, best=i)
+    pred <- predict(prunedTree, newdata=validation, type="tree")
+    train_score[i] <- deviance(prunedTree)
+    validation_score[i] <- deviance(pred)
+}
+## ---- end-of-assign2-3
+
+## ---- assign2-3-score
+plot_data <- data.frame(Leaves=leaves, Train=train_score[leaves], Validation=validation_score[leaves])
+plot_data <- melt(plot_data, id="Leaves", value.name="Deviance", variable.name="Set")
+
+ggplot() +
+    geom_line(data=plot_data, aes(x=Leaves, y=Deviance, color=Set)) +
+    scale_x_continuous(breaks=leaves)
+## ---- end-of-assign2-3-score
+
+## ---- assign2-3-optimal
+optimal_leaves <- which.min(validation_score[leaves]) + 1
+optimal_tree <- prune.tree(dtreefit, best=optimal_leaves)
+prediction.tree(optimal_tree, test[, -ncol(test)], test$good_bad)
+## ---- end-of-assign2-3-optimal
+
+## ---- assign2-3-plot
+plot(optimal_tree)
+text(optimal_tree, pretty=0)
+## ---- end-of-assign2-3-plot
+
+## 4
+## ---- assign2-4
+prediction.bayes <- function(model, X, y, loss) {
+    predicted <- predict(model, X, type="raw")
+    predicted <- factor(ifelse(predicted[, 1] / predicted[, 2] > loss, 0, 1),
+                        levels=c(0, 1), labels=c("bad", "good"))
+
+    confusion_matrix <- table(pred=predicted, true=y)
+    list(confusion_matrix=confusion_matrix,
+         misclassification_rate= 1 - sum(diag(confusion_matrix)) / sum (confusion_matrix))
+}
+
+bayesfit <- naiveBayes(good_bad ~ ., data=train)
+## ---- end-of-assign2-4
+
+## ---- assign2-4-train
+prediction.bayes(bayesfit, train[, -ncol(train)], train$good_bad, 1)
+## ---- end-of-assign2-4-train
+
+## ---- assign2-4-test
+prediction.bayes(bayesfit, test[, -ncol(test)], test$good_bad, 1)
+## ---- end-of-assign2-4-test
+
+## 5
+## ---- assign2-5
+bayesfit <- naiveBayes(good_bad ~ ., data=train)
+## ---- end-of-assign2-5
+
+## ---- assign2-5-train
+prediction.bayes(bayesfit, train[, -ncol(train)], train$good_bad,  1 / 10)
+## ---- end-of-assign2-5-train
+
+## ---- assign2-5-test
+prediction.bayes(bayesfit, test[, -ncol(test)], test$good_bad, 1 / 10)
+## ---- end-of-assign2-5-test
