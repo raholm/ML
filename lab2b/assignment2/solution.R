5c4491ac1bb092a12bb36eebb3b38e06a8b23070
Rasmus Holm <rasmus.holm723@gmail.com>
2016-11-29 17:51:55 +0100

    Done most of assignment 2 and 4. Started on the report.

--- lab2b/assignment2/solution.R
+++ lab2b/assignment2/solution.R
@@ -3,14 +3,75 @@ library(tree)
 
 data <- read.csv2("../data/bodyfatregression.csv")
 names(data) <- c("Waist", "Weight", "Bodyfat")
+
+set.seed(1234567890)
+train_idx <- sample(nrow(data), floor(nrow(data) * (2 / 3)))
+train <- data[train_idx,]
+test <- data[-train_idx,]
 ## ---- end-of-assign2-init
 
 ## 1
-fit <- tree(Bodyfat ~ Waist + Weight, data=data)
+set.seed(1234567890)
+
+tree_count <- 100
+test_errors <- rep(0, tree_count)
+
+for (i in 1:tree_count) {
+    newdata <- train[sample(nrow(train), replace=TRUE),]
+    fit <- tree(Bodyfat ~ ., data=newdata, split="deviance")
+    test_error <- sum(predict(fit, test) - test$Bodyfat)
+    test_errors[i] <- test_error
+}
+
+mean((test_errors / tree_count)^2)
 
 ## 2
+set.seed(1234567890)
+
+tree_count <- 100
+errors <- rep(0, tree_count)
+
+for (i in 1:tree_count) {
+    newdata <- data[sample(nrow(data), replace=TRUE),]
+    fit <- tree(Bodyfat ~ ., data=newdata, split="deviance")
+    fit.cv <- cv.tree(fit, K=3)
+
+    optimal_size <- fit.cv$size[which.min(fit.cv$dev)]
+    fit <- prune.tree(fit, best=optimal_size)
+    
+    error <- sum(predict(fit, data) - data$Bodyfat)
+    errors[i] <- error
+}
+
+mean((errors / tree_count)^2)
+
 
 ## 3
 
+## Bagging Regression Trees
+set.seed(1234567890)
+trees <- list()
 
+for (i in 1:tree_count) {
+    newdata <- data[sample(nrow(data), replace=TRUE),]
+    fit <- tree(Bodyfat ~ ., data=newdata, split="deviance")
+    trees[[i]] <- fit
+}
+
+length(trees)
+
+
+## Bagging CV Regression Trees
+set.seed(1234567890)
+trees <- list()
+
+for (i in 1:tree_count) {
+    newdata <- data[sample(nrow(data), replace=TRUE),]
+    fit <- tree(Bodyfat ~ ., data=newdata, split="deviance")
+    fit.cv <- cv.tree(fit, K=3)
+
+    optimal_size <- fit.cv$size[which.min(fit.cv$dev)]
+    fit <- prune.tree(fit, best=optimal_size)
+    trees[[i]] <- fit
+}
 
--- lab2b/assignment4/solution.R
+++ lab2b/assignment4/solution.R
@@ -1,7 +1,64 @@
 ## ---- assign4-init
 library(mboost)
 library(randomForest)
+library(ggplot2)
+library(reshape2)
 
 data <- read.csv2("../data/spambase.csv")
+data$Spam <- as.factor(data$Spam)
+
+set.seed(1234567890)
+train_idx <- sample(nrow(data), floor(nrow(data) * (2 / 3)))
+train <- data[train_idx,]
+test <- data[-train_idx,]
 ## ---- end-of-assign4-init
 
+## ---- assign4-tree
+tree_counts <- seq(10, 100, by=10)
+test_errors <- rep(0, length(tree_counts))
+train_errors <- rep(0, length(tree_counts))
+
+for (i in 1:length(tree_counts)) {
+    fit <- blackboost(Spam ~ ., data=train, family=AdaExp(),
+                      control=boost_control(mstop=tree_counts[i]))
+    test_error <- 1 - sum(predict(fit, test, type="class") == test$Spam) / nrow(test)
+    train_error <- 1 - sum(predict(fit, type="class") == train$Spam) / nrow(train)
+    test_errors[i] <- test_error
+    train_errors[i] <- train_error
+}
+## ---- end-of-assign4-tree
+
+## ---- assign4-tree-plot
+plot_data <- data.frame(Trees=tree_counts, test=test_errors, train=train_errors)
+plot_data <- melt(plot_data, id="Trees", value.name="Error", variable.name="Data")
+
+ggplot(plot_data) +
+    xlab("Number of Regression Trees") +
+    ylab("Misclassification Rate") +
+    geom_line(aes(x=Trees, y=Error, color=Data)) +
+    scale_x_discrete(limits=tree_counts)
+## ---- end-of-assign4-tree-plot
+
+## ---- assign4-forest
+test_errors <- rep(0, length(tree_counts))
+train_errors <- rep(0, length(tree_counts))
+
+for (i in 1:length(tree_counts)) {
+    fit <- randomForest(Spam ~ ., data=train, ntree=tree_counts[i])
+    test_error <- 1 - sum(predict(fit, test, type="class") == test$Spam) / nrow(test)
+    train_error <- 1 - sum(predict(fit, type="class") == train$Spam) / nrow(train)
+    test_errors[i] <- test_error
+    train_errors[i] <- train_error
+}
+## ---- end-of-assign4-forest
+
+## ---- assign4-forest-plot
+plot_data <- data.frame(Trees=tree_counts, test=test_errors, train=train_errors)
+plot_data <- melt(plot_data, id="Trees", value.name="Error", variable.name="Data")
+
+ggplot(plot_data) +
+    xlab("Number of Regression Trees") +
+    ylab("Misclassification Rate") +
+    geom_line(aes(x=Trees, y=Error, color=Data)) +
+    scale_x_discrete(limits=tree_counts)
+## ---- end-of-assign4-forest-plot
