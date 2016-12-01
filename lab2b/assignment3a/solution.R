b9bcfc5b4f194ce0c95ea9433596f290e7ea87ac
Rasmus Holm <rasmus.holm723@gmail.com>
2016-12-01 12:56:00 +0100

    Wrote code for assignment 3.

--- lab2b/assignment3a/solution.R
+++ lab2b/assignment3a/solution.R
@@ -1,3 +1,29 @@
 ## ---- assign3a-init
+library(mboost)
 
+data <- read.csv2("../data/bodyfatregression.csv")
+
+fit <- blackboost(Bodyfat_percent ~ Waist_cm + Weight_kg, data=data)
+
+cvf <- cv(model.weights(fit), type="kfold")
+cvm <- cvrisk(fit, folds=cvf, grid=1:100)
 ## ---- end-of-assign3a-init
+
+## ---- assign3a-plot
+plot(cvm)
+## ---- end-of-assign3a-plot
+
+## ---- assign3a-2
+set.seed(1234567890)
+train_idx <- sample(nrow(data), floor(nrow(data) * (2 / 3)))
+train <- data[train_idx,]
+test <- data[-train_idx,]
+
+fit <- blackboost(Bodyfat_percent ~ Waist_cm + Weight_kg, data=train,
+                  control=boost_control(mstop=mstop(cvm)))
+test_error <- sum((predict(fit, test) - test$Bodyfat_percent)^2)
+train_error <- sum((predict(fit, train) - train$Bodyfat_percent)^2)
+
+test_error
+train_error
+## ---- end-of-assign3a-2
