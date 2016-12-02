31a8b8d234f2d5975d6c164985bbae32dffec508
Rasmus Holm <rasmus.holm723@gmail.com>
2016-12-02 15:12:26 +0100

    Fixed 2.2.

--- lab2b/assignment2a/solution.R
+++ lab2b/assignment2a/solution.R
@@ -3,15 +3,15 @@ library(tree)
 
 data <- read.csv2("../data/bodyfatregression.csv")
 names(data) <- c("Waist", "Weight", "Bodyfat")
-
-set.seed(1234567890)
-train_idx <- sample(nrow(data), floor(nrow(data) * (2 / 3)))
-train <- data[train_idx,]
-test <- data[-train_idx,]
 ## ---- end-of-assign2a-init
 
 ## 1
 ## ---- assign2a-1-lowerbound
+set.seed(1234567890)
+train_idx <- sample(nrow(data), floor(nrow(data) * (2 / 3)))
+train <- data[train_idx,]
+test <- data[-train_idx,]
+
 set.seed(1234567890)
 
 tree_count <- 100
@@ -30,6 +30,11 @@ mean((test_errors / length(test_errors))^2)
 
 ## ---- assign2a-1-upperbound
 set.seed(1234567890)
+train_idx <- sample(nrow(data), floor(nrow(data) * (2 / 3)))
+train <- data[train_idx,]
+test <- data[-train_idx,]
+
+set.seed(1234567890)
 
 tree_count <- 100
 test_errors <- rep(0, tree_count)
@@ -37,6 +42,7 @@ test_errors <- rep(0, tree_count)
 for (i in 1:tree_count) {
     newdata <- train[sample(nrow(train), replace=TRUE),]
     fit <- tree(Bodyfat ~ ., data=newdata, split="deviance")
+
     test_error <- mean((predict(fit, test) - test$Bodyfat)^2)
     test_errors[i] <- test_error
 }
@@ -46,54 +52,44 @@ mean(test_errors)
 
 ## 2
 ## ---- assign2a-2-upperbound
+tree_count <- 100
+fold_count <- 3
+test_errors <- matrix(0, nrow=tree_count, ncol=fold_count)
+
 set.seed(1234567890)
 
-tree_count <- 100
-k <- 3
-errors <- rep(0, tree_count * k)
+folds <- suppressWarnings(split(nrow(data), f=1:fold_count))
 
-for (i in 1:tree_count) {
-    newdata <- data[sample(nrow(data), replace=TRUE),]
-    datasets <- suppressWarnings(split(newdata, 1:k))
+for (j in 1:fold_count) {
+    train <- data[-folds[[j]],]
+    test <- data[folds[[j]],]
 
-    train1 <- rbind(datasets[[1]], datasets[[2]])
-    test1 <- datasets[[3]]
+    for (i in 1:tree_count) {
+        newdata <- train[sample(nrow(train), replace=TRUE),]
+        fit <- tree(Bodyfat ~ ., data=newdata, split="deviance")
 
-    train2 <- rbind(datasets[[1]], datasets[[3]])
-    test2 <- datasets[[2]]
-
-    train3 <- rbind(datasets[[2]], datasets[[3]])
-    test3 <- datasets[[1]]
-    
-    fit1 <- tree(Bodyfat ~ ., data=train1, split="deviance")
-    error1 <- mean((predict(fit1, test1) - test1$Bodyfat)^2)
-
-    fit2 <- tree(Bodyfat ~ ., data=train2, split="deviance")
-    error2 <- mean((predict(fit2, test2) - test2$Bodyfat)^2)
-
-    fit3 <- tree(Bodyfat ~ ., data=train3, split="deviance")
-    error3 <- mean((predict(fit3, test3) - test3$Bodyfat)^2)
-
-    errors[(i - 1) * k + 1] <- error1
-    errors[(i - 1) * k + 2] <- error2
-    errors[(i - 1) * k + 3] <- error3
+        test_error <- mean((predict(fit, test) - test$Bodyfat)^2)
+        test_errors[i, j] <- test_error
+    }
 }
 
-mean(errors)
+mean(test_errors)
 ## ---- end-of-assign2a-2-upperbound
 
 ## 3
 ## ---- assign2a-3-bag
 bagging.regtrees <- function(formula, data, newdata, b) {
     predictions <- matrix(0, nrow=nrow(newdata), ncol=b)
+    trees <- list()
 
-    for (i in 1:k) {
+    for (i in 1:b) {
         bootstrap_sample <- data[sample(nrow(data), replace=TRUE),]
         fit <- tree(formula, data=bootstrap_sample, split="deviance")
+        trees[[i]] <- fit
         predictions[, i] <- predict(fit, newdata)
     }
 
-    rowMeans(predictions)
+    list(trees=trees, predictions=rowMeans(predictions))
 }
 ## ---- end-of-assign2a-3-bag
 
@@ -101,6 +97,7 @@ bagging.regtrees <- function(formula, data, newdata, b) {
 cv.regtrees <- function(formula, data, newdata, b, k) {
     predictions <- matrix(0, nrow(nrow(newdata)), ncol=b*k)
 
+    
     for (i in 1:tree_count) {
         bootstrap_sample <- data[sample(nrow(data), replace=TRUE),]
         datasets <- suppressWarnings(split(bootstrap_sample, 1:k))
