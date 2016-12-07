bea05311f12716453dbdb343d5fead6b194d2ee6
raholm <rasmus.holm723@gmail.com>
2016-12-08 00:13:40 +0100

    Moved around the code.

--- lab5/assignment1/solution.R
+++ lab5/assignment1/solution.R
@@ -11,20 +11,6 @@ temps <- read.csv("../data/temps50k.csv", stringsAsFactors=FALSE)
 st <- merge(stations, temps, by="station_number")
 data <- st[, c("longitude", "latitude", "date", "time", "air_temperature")]
 
-h_distance <- 1000 # These three values are up to the students
-h_date <- 10
-h_time <- 2
-
-pred_latitude <- 58.409158 # The point to predict (up to the students)
-pred_longitude <- 15.607452
-pred_date <- "2013-06-24" # The date to predict (up to the students)
-pred_times <- c("02:00:00", "04:00:00", "06:00:00", "08:00:00",
-                "10:00:00", "12:00:00", "14:00:00", "16:00:00",
-                "18:00:00", "20:00:00", "22:00:00", "24:00:00")
-
-pred_temp <- vector(length=length(pred_times))
-
-## Students’ code here
 gaussian.kernel <- function(u) {
     exp(-u^2)
 }
@@ -51,7 +37,7 @@ time.kernel <- function(X, time, h) {
 
 ## time.pred(st_filtered, pred_times[1], h_time)
 
-filter <- function(X, date, time) {
+filter_by_date <- function(X, date, time) {
     complete_dates <- paste(X$date, X$time)
     complete_dates <- as.POSIXct(complete_dates, format="%Y-%m-%d %H:%M:%S")
 
@@ -67,20 +53,35 @@ filter <- function(X, date, time) {
 }
 
 kernel.model <- function(X, lat, long, h_dist, date, h_date, time, h_time) {
-    X <- filter(X, date, time)
+    X <- filter_by_date(X, date, time)
     date <- as.Date(date)
     time <- as.POSIXct(time, format="%H:%M:%S")
     kernel <- distance.kernel(X, lat, long, h_dist) + date.kernel(X, date, h_date) + time.kernel(X, time, h_time)
     sum(kernel * X$air_temperature) / sum(kernel)
 }
+## ---- end-of-assign1-init
+
+## ---- assign1-run
+h_distance <- 100000 # These three values are up to the students
+h_date <- 7
+h_time <- 4
+
+pred_latitude <- 58.409158 # The point to predict (up to the students)
+pred_longitude <- 15.607452
+pred_date <- "2000-07-24" # The date to predict (up to the students)
+pred_times <- c("02:00:00", "04:00:00", "06:00:00", "08:00:00",
+                "10:00:00", "12:00:00", "14:00:00", "16:00:00",
+                "18:00:00", "20:00:00", "22:00:00", "24:00:00")
+
+pred_temp <- vector(length=length(pred_times))
 
 for (i in 1:length(pred_times)) {
     pred_temp[i] <- kernel.model(data, pred_latitude, pred_longitude, h_distance,
-                                 pred_date, h_date, pred_times[i], h_time) / 3
+                                 pred_date, h_date, pred_times[i], h_time)
 }
 
 x_breaks <- seq(1, length(pred_times), 2)
 plot(y=pred_temp, x=1:length(pred_times), type="o", xaxt = "n",
      xlab="Time", ylab="Estimated Temperature")
 axis(1, at=x_breaks, labels=pred_times[x_breaks])
-## ---- end-of-assign1-init
+## ---- end-of-assign1-run
