074476afc7ac4c254a397311bcee2940c9eecfa7
Rasmus Holm <rasmus.holm723@gmail.com>
2016-12-07 15:58:53 +0100

    Rework calculation of the kernels.

--- lab5/assignment1/solution.R
+++ lab5/assignment1/solution.R
@@ -11,13 +11,13 @@ temps <- read.csv("../data/temps50k.csv", stringsAsFactors=FALSE)
 st <- merge(stations, temps, by="station_number")
 data <- st[, c("longitude", "latitude", "date", "time", "air_temperature")]
 
-h_distance <- 10000 # These three values are up to the students
-h_date <- 7
-h_time <- 3
+h_distance <- 1000 # These three values are up to the students
+h_date <- 10
+h_time <- 4
 
 pred_latitude <- 58.409158 # The point to predict (up to the students)
 pred_longitude <- 15.607452
-pred_date <- "2016-12-24" # The date to predict (up to the students)
+pred_date <- "2013-06-24" # The date to predict (up to the students)
 pred_times <- c("02:00:00", "04:00:00", "06:00:00", "08:00:00",
                 "10:00:00", "12:00:00", "14:00:00", "16:00:00",
                 "18:00:00", "20:00:00", "22:00:00", "24:00:00")
@@ -29,30 +29,25 @@ gaussian.kernel <- function(u) {
     exp(-u^2)
 }
 
-distance.pred <- function(X, lat, long, h) {
+distance.kernel <- function(X, lat, long, h) {
     distances <- distHaversine(X[, c("longitude", "latitude")],
                                c(long, lat))
 
-    weights <- gaussian.kernel(distances / h)
-    sum(X$air_temperature * weights / sum(weights))
+    gaussian.kernel(distances / h)
 }
 
 ## distance.pred(st_filtered, pred_latitude, pred_longitude, h_distance)
 
-date.pred <- function(X, date, h) {
+date.kernel <- function(X, date, h) {
     distances <- as.numeric(difftime(X$date, date, units="days"))
-    weights <- gaussian.kernel(distances / h)
-    sum(X$air_temperature * weights / sum(weights))
+    gaussian.kernel(distances / h)
 }
 
 ## date.pred(st_filtered, pred_date, h_date)
 
-time.pred <- function(X, time, h) {
+time.kernel <- function(X, time, h) {
     distances <- abs(as.numeric(difftime(X$time, time, units="hours")))
-
-    weights <- gaussian.kernel(distances / h)
-
-    sum(X$air_temperature * weights / sum(weights))
+    gaussian.kernel(distances / h)
 }
 
 ## time.pred(st_filtered, pred_times[1], h_time)
@@ -73,15 +68,16 @@ filter <- function(X, date, time) {
 }
 
 kernel.model <- function(X, lat, long, h_dist, date, h_date, time, h_time) {
-    date <- as.Date(date)
     X <- filter(X, date, time)
+    date <- as.Date(date)
     time <- as.POSIXct(time, format="%H:%M:%S")
-    distance.pred(X, lat, long, h_dist) + date.pred(X, date, h_date) + time.pred(X, time, h_time)
+    kernel <- distance.kernel(X, lat, long, h_dist) + date.kernel(X, date, h_date) + time.kernel(X, time, h_time)
+    sum(kernel * X$air_temperature) / sum(kernel)
 }
 
 for (i in 1:length(pred_times)) {
     pred_temp[i] <- kernel.model(data, pred_latitude, pred_longitude, h_distance,
-                                 pred_date, h_date, pred_times[i], h_time)
+                                 pred_date, h_date, pred_times[i], h_time) / 3
 }
 
 x_breaks <- seq(1, length(pred_times), 2)
