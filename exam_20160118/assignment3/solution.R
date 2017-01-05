e74356a9502aa87e59d07089decf5f10add305a7
raholm <rasmus.holm723@gmail.com>
2017-01-06 00:44:45 +0100

    Plot stuff.

--- exam_20160118/assignment3/solution.R
+++ exam_20160118/assignment3/solution.R
@@ -8,11 +8,11 @@ train_idx <- sample(1:nrow(data), size=floor(nrow(data) * 0.7))
 train <- data[train_idx,]
 test <- data[-train_idx,]
 
-
 ## 3
 set.seed(12345)
 formula <- paste("class ~ ", paste(names(data)[-1], collapse=" + "))
 fit <- neuralnet(formula=formula, data=train, hidden=0, act.fct="tanh", linear.output=FALSE)
+plot(fit)
 
 weights <- fit$weights[[1]][[1]][-1,]
 weights
@@ -30,6 +30,7 @@ test_error
 set.seed(12345)
 formula <- paste("class ~ ", paste(names(data)[-1], collapse=" + "))
 fit <- neuralnet(formula=formula, data=train, hidden=1, act.fct="tanh", linear.output=TRUE)
+plot(fit)
 
 train_error <- mean(sign(compute(fit, train[, -1])$net.result) != train$class)
 train_error
@@ -38,3 +39,6 @@ test_error <- mean(sign(compute(fit, test[, -1])$net.result) != test$class)
 test_error
 
 ## 6
+## 1. A tanh function
+## 2. A translated tanh function
+## 3. Parabola
