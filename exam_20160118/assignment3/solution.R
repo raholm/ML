f8075d23f05c48e7d6986cff97cc4d0957f61f5a
raholm <rasmus.holm723@gmail.com>
2017-01-06 00:24:43 +0100

    Assignment 3.

---
+++ exam_20160118/assignment3/solution.R
@@ -0,0 +1,40 @@
+library(neuralnet)
+
+data <- read.csv("../data/wine.csv")
+data$class[which(data$class == 2)] <- -1
+
+set.seed(12345)
+train_idx <- sample(1:nrow(data), size=floor(nrow(data) * 0.7))
+train <- data[train_idx,]
+test <- data[-train_idx,]
+
+
+## 3
+set.seed(12345)
+formula <- paste("class ~ ", paste(names(data)[-1], collapse=" + "))
+fit <- neuralnet(formula=formula, data=train, hidden=0, act.fct="tanh", linear.output=FALSE)
+
+weights <- fit$weights[[1]][[1]][-1,]
+weights
+variables <- fit$model.list$variables[order(abs(weights), decreasing=TRUE)]
+variables
+
+## 4
+train_error <- mean(sign(compute(fit, train[, -1])$net.result) != train$class)
+train_error
+
+test_error <- mean(sign(compute(fit, test[, -1])$net.result) != test$class)
+test_error
+
+## 5
+set.seed(12345)
+formula <- paste("class ~ ", paste(names(data)[-1], collapse=" + "))
+fit <- neuralnet(formula=formula, data=train, hidden=1, act.fct="tanh", linear.output=TRUE)
+
+train_error <- mean(sign(compute(fit, train[, -1])$net.result) != train$class)
+train_error
+
+test_error <- mean(sign(compute(fit, test[, -1])$net.result) != test$class)
+test_error
+
+## 6
