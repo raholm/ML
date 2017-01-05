668d5911cd5d16d04d5cc987184c3657698d1c05
raholm <rasmus.holm723@gmail.com>
2017-01-05 22:04:12 +0100

    Started on the test exam.

---
+++ exam_20160118/assignment1/solution.R
@@ -0,0 +1,70 @@
+library(tree)
+library(ggplot2)
+library(pls)
+
+data <- read.csv2("../data/glass.csv")
+
+set.seed(12345)
+n <- nrow(data)
+
+train_size <- floor(n * 0.5)
+validation_size <- floor(n * 0.25)
+test_size <- n - train_size - validation_size
+
+idx <- 1:n
+train_idx <- sample(x=idx, size=train_size)
+validation_idx <- sample(x=idx[-train_idx], size=validation_size)
+test_idx <- idx[-c(train_idx, validation_idx)]
+
+train <- data[train_idx,]
+validation <- data[validation_idx,]
+test <- data[test_idx,]
+
+## 1
+sizes <- 2:8
+validation_errors <- rep(0, length(sizes))
+fit <- tree(Al ~ ., data=train)
+
+for (size in sizes) {
+    fit_pruned <- prune.tree(fit, best=size)
+    validation_errors[size-1] <- mean((predict(fit_pruned, newdata=validation) - validation$Al)^2)
+}
+
+plot_data <- data.frame(x=sizes, y=validation_errors)
+
+ggplot() +
+    xlab("# of terminal nodes") + ylab("Mean Squarred Error") +
+    geom_point(data=plot_data, aes(x=x, y=y))
+
+## 2
+optimal_size <- which.min(validation_errors) + 1
+optimal_tree <- prune.tree(fit, best=optimal_size)
+test_error <- mean((predict(optimal_tree, newdata=test) - test$Al)^2)
+test_error
+
+plot(optimal_tree)
+text(optimal_tree, pretty=TRUE)
+
+## 3
+set.seed(12345)
+fit <- plsr(Al ~ ., data=train, validation="CV")
+
+summary(fit)
+fit$validation
+fit$scores
+fit$loadings
+
+optimal_fit <- plsr(Al ~ ., data=train, ncomp=6)
+
+## a) 3 variables
+## b) 6 variables
+## c) According to CV the model with 6 components is best
+## d) Na Mg Si Ca Ba
+## e) Y_score = z1 + z2 + z3 + z4 + z5 + z6
+rowSums(optimal_fit$scores)
+
+## f)
+test_error <- mean((predict(optimal_fit, newdata=test) - test$Al)^2)
+test_error
+
+## 4
---
+++ exam_20160118/assignment2/solution.R
@@ -0,0 +1,73 @@
+library(ggplot2)
+library(MASS)
+
+data <- mtcars
+data$shp <- scale(data$hp)
+data$sqsec <- scale(data$qsec)
+data$am <- as.factor(data$am)
+
+## 1
+ggplot() +
+    geom_point(data=data, aes(x=shp, y=sqsec, color=am))
+
+## No, the data is not linearly separable
+
+## 2
+prior <- c(1, 1) / 2
+fit_eq <- lda(am ~ shp + sqsec, data=data, prior=prior)
+fit_eq
+
+prior <- as.numeric(table(data$am) / sum(table(data$am)))
+fit_neq <- lda(am ~ shp + sqsec, data=data, prior=prior)
+fit_neq
+
+prediction_eq <- predict(fit_eq, data)$class
+prediction_neq <- predict(fit_neq, data)$class
+
+plot_data1 <- data.frame(x=data$shp, y=data$sqsec, color=prediction_eq, type="eq")
+plot_data2 <- data.frame(x=data$shp, y=data$sqsec, color=prediction_neq, type="neq")
+
+plot_data <- rbind(plot_data1, plot_data2)
+
+ggplot() +
+    geom_point(data=plot_data, aes(x=x, y=y, color=color)) +
+    facet_grid(type ~ .)
+
+## 3
+euclidean <- function(u) {
+    sqrt(sum(u^2))
+}
+
+kernel.epan <- function(u) {
+    (1 - euclidean(u)^2) * as.numeric((euclidean(u) <= 1))
+}
+
+kernel.density <- function(X, Xtest, lambda) {
+    apply(Xtest, 1, function(x){
+        s <- 0
+        for (i in 1:nrow(X)) {
+            s <- s + kernel.epan((X[i, ] - x) / lambda)
+        }
+
+        s / nrow(X)
+    })
+}
+
+lambda <- 0.2
+
+idx1 <- which(data$am == 0)
+X1 <- as.matrix(data.frame(data$qsec[idx1], data$hp[idx1]))
+Xtest1 <- as.matrix(data.frame(data$qsec, data$hp))
+
+density1 <- kernel.density(X1, Xtest1, lambda)
+
+idx2 <- which(data$am == 1)
+X2 <- data.frame(data$qsec[idx2], data$hp[idx2])
+Xtest2 <- data.frame(data$qsec, data$hp)
+density2 <- kernel.density(X2, Xtest2, lambda)
+
+densities <- data.frame(density1, density2)
+prediction <- apply(densities, 1, function(x) which.max(x))
+
+prediction_error <- mean(as.numeric(data$am) != prediction)
+prediction_error
